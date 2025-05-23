fe1a2ecd5a9c5c02012394d068ead4cd417ca7d9
LeenVdenB <r1060160@student.thomasmore.be>
2025-05-23 16:48:23 +0200

    feat: added total sale revenue to local storage + sale history in html

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ import SaleManager from "./classes/SaleManager.js";
 import StockItem from "./classes/StockItem.js";
 import StockManager from "./classes/StockManager.js";
 
-let stroStock = {};
-let hooiStock = {};
-
 let saleManager = new SaleManager();
 saleManager.loadSales();
 let stockManager = new StockManager();
@@ -22,12 +19,17 @@ if (document.querySelector(".voorraadContainer")) {
 
 if (document.querySelector("#saveSale")) {
   document.querySelector("#saveSale").addEventListener("click", (e) => {
-    e.preventDefault;
+    e.preventDefault();
     let klant = document.querySelector("#person").value;
     let soldHooi = document.querySelector("#sellHooi").value;
     let soldStro = document.querySelector("#sellStro").value;
 
-    let sale = new Sale(klant, soldHooi, soldStro);
+    let hooiStock = stockManager.getStockItem("hooi");
+    let stroStock = stockManager.getStockItem("stro");
+    let priceHooi = hooiStock ? hooiStock.price : 0;
+    let priceStro = stroStock ? stroStock.price : 0;
+
+    let sale = new Sale(klant, soldHooi, soldStro, priceHooi, priceStro);
     saleManager.addSale(sale);
 
     stockManager.reduceStock("hooi", soldHooi);
@@ -36,9 +38,27 @@ if (document.querySelector("#saveSale")) {
   });
 }
 
+let saleGeschiedenisContainer = document.querySelector("#saleGeschiedenis");
+if (saleGeschiedenisContainer) {
+  let sales = saleManager.getSales();
+  let ul = document.createElement("ul");
+
+  sales.forEach((sale) => {
+    let li = document.createElement("li");
+    let saleDateTime = new Date(sale.datum).toLocaleString();
+    li.textContent = `${saleDateTime} - ${sale.klant} kocht ${
+      sale.soldHooi
+    } balen hooi en ${
+      sale.soldStro
+    } balen stro. Totale opbrengst: €${sale.totalPrice.toFixed(2)}`;
+    ul.appendChild(li);
+  });
+  saleGeschiedenisContainer.appendChild(ul);
+}
+
 if (document.querySelector("#save")) {
   document.querySelector("#save").addEventListener("click", (e) => {
-    e.preventDefault;
+    e.preventDefault();
     let stroAmount = document.querySelector("#stroAmount").value;
     let priceStro = document.querySelector("#priceStro").value;
     let hooiAmount = document.querySelector("#hooiAmount").value;
--- classes/Sale.js
+++ classes/Sale.js
@@ -1,9 +1,15 @@
 export default class Sale {
-  constructor(klant, soldHooi, soldStro) {
+  constructor(klant, soldHooi, soldStro, priceHooi, priceStro) {
     this.klant = klant;
     this.soldHooi = Number(soldHooi) || 0;
     this.soldStro = Number(soldStro) || 0;
+    this.priceHooi = Number(priceHooi) || 0;
+    this.priceStro = Number(priceStro) || 0;
     this.datum = new Date();
+  }
+
+  get totalPrice() {
+    return this.soldHooi * this.priceHooi + this.soldStro * this.priceStro;
   }
 
   toJSON() {
@@ -11,12 +17,20 @@ export default class Sale {
       klant: this.klant,
       soldHooi: this.soldHooi,
       soldStro: this.soldStro,
+      priceHooi: this.priceHooi,
+      priceStro: this.priceStro,
       datum: this.datum,
     };
   }
 
   static fromJSON(data) {
-    let sale = new Sale(data.klant, data.soldHooi, data.soldStro);
+    let sale = new Sale(
+      data.klant,
+      data.soldHooi,
+      data.soldStro,
+      data.priceHooi,
+      data.priceStro
+    );
     sale.datum = new Date(data.datum);
     return sale;
   }
